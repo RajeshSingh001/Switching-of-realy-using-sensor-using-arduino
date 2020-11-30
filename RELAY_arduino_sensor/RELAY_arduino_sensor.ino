81dd35994036fbab3c4d3068337143b9650b4707
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-30 10:06:13 +0530

    code and proteus model

---
+++ RELAY_arduino_sensor/RELAY_arduino_sensor.ino
@@ -0,0 +1,30 @@
+
+const int FS = 5; 
+int BULB1 = 10;
+int FSState = 0; 
+void setup()
+{                
+  // initialize the digital pin as an output.
+  pinMode(BULB1, OUTPUT);
+  pinMode(FS, INPUT);
+ // pinMode(led2, OUTPUT);
+}
+
+void loop()
+{
+  FSState = digitalRead(FS); 
+  
+  if (FSState == HIGH)
+  {  
+  digitalWrite(BULB1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //digitalWrite(led2, HIGH); 
+  delay(20); 
+  } 
+  
+  else 
+  {
+  digitalWrite(BULB1, LOW);
+  delay(20); 
+  }
+
+}
